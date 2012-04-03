f32b56d6fb197596869d631bf6c051b4ba486933
sunnylost <sunnylost@gmail.com>
2012-04-03 23:12:02 +0800

    +增加了猴子搜索，半成品 不在状态呀~好些天没更新了~等放假结束再调整回来~

--- MonkeyBean.js
+++ MonkeyBean.js
@@ -228,8 +228,8 @@ typeof Updater != 'undefined' && new Updater({
             return str && str.split(':')[0];
         },
 
-        getCk : function() {
-            return this.ck || (this.ck = cookie.get('ck'));
+        getCk : function(flag) {
+            return !flag ? (this.ck || (this.ck = cookie.get('ck'))) : cookie.get('ck');
         },
         //Mr.TM Tripple M;
         MonkeyModuleManager : (function() {
@@ -1177,7 +1177,7 @@ typeof Updater != 'undefined' && new Updater({
                                     <ul>\
                                         <li><a href="http://www.douban.com/doumail/">豆邮</a></li>\
                                         <li><a href="javascript:void(0);" monkey-action="MonkeyConfig.config" title="MonkeyBean插件设置">MonkeyBean</a></li>\
-                                        <li><a href="http://www.douban.com/accounts/logout?ck=9P95">退出</a></li>\
+                                        <li><a href="http://www.douban.com/accounts/logout?ck=' + MonkeyBean.getCk() + '">退出</a></li>\
                                     </ul>\
                                 </li>\
                             </ul>\
@@ -2399,7 +2399,117 @@ typeof Updater != 'undefined' && new Updater({
     });
 
     /**
-     * 猴子底部工具栏——提供回到顶部、回到底部、豆瓣说等功能
+     * 猴子搜索——多个网站的快捷搜索方式
+     * updateTime : 2012-4-3
+     */
+    MonkeyModule('MonkeySearch', {
+        css : '#MonkeySearch {\
+                    position: relative;\
+                }\
+                #MonkeySearch span{\
+                    display: inline-block;\
+                    margin-bottom: 10px;\
+               }\
+               #MonkeySearchList {\
+                    list-style: none;\
+                    width: 180px;\
+                    display: none;\
+                    position: absolute;\
+                    top: 22px;\
+                    overflow: hidden;\
+                    background-color: #fff;\
+                    border-radius: 5px;\
+                    border: 1px solid #CCCCCC;\
+               }\
+               #MonkeySearchList li {\
+                    padding: 4px;\
+                    border-radius: 5px;\
+                    float: left;\
+                    width: 50px;\
+               }',
+
+        html : '<div id="MonkeySearch">\
+                    <span id="MonkeySearchBtn" class="Monkey-Button">\
+                        网站搜索\
+                    </span>\
+                </div>',
+
+        fit : function() {
+            var type = MonkeyBean.page.type;
+            return (type == 'book' || type == 'movie' || type == 'music') && MonkeyBean.path.indexOf('subject') != -1;
+        },
+
+        load : function() {
+            var that = this;
+            this.render();
+            $('#MonkeySearchBtn').hover(function() {
+                clearTimeout(that.ID);
+                !that.isShow && that.show();
+            }, function() {
+                that.ID = setTimeout(function(e) {
+                    var flag = $.contains(that.el[0], e.relatedTarget);
+                    clearTimeout(that.ID);
+                    !flag && that.hide();
+                }, 100);
+            })
+            this.list = $('#MonkeySearchList');
+            this.list.hover(function() {
+                clearTimeout(that.ID);
+                that.isShown = true;
+            }, function() {
+                that.hide();
+            })
+        },
+
+        render : function() {
+            GM_addStyle(this.css);
+            this.el = $(this.html);
+            $('.aside').prepend(this.el);
+            //var list = MonkeyBean.get('MonkeySearchList');
+            this.el.append('<ul id="MonkeySearchList">\
+                                <li><a href="http://www.google.com.hk/search?&q={word}">Google</a></li>\
+                                <li><a href="http://www.baidu.com/s?ie=utf-8&wd={word}">百度一下</a></li>\
+                                <li><a href="http://www.verycd.com/search/folders/{word}">VeryCD</a></li>\
+                                <li><a href="http://zh.wikipedia.org/w/index.php?search={word}">维基百科</a></li>\
+                                <li><a href="http://books.google.com/books?q={word}">谷歌书籍</a></li>\
+                                <li><a href="http://shooter.cn/search/{word}">射手字幕</a></li>\
+                                <li><a href="http://ishare.iask.sina.com.cn/search.php?key={gb:word}">新浪爱问</a></li>\
+                                <li><a href="http://wenku.baidu.com/search?word={gb:word}">百度文库</a></li>\
+                                <li><a href="http://so.tudou.com/psearch/{word}">土豆豆单</a></li>\
+                                <li><a href="http://www.douban.com/group/search?q={word}">百度文库</a></li>\
+                                <li><a href="http://www.douban.com/group/search?q={word}">土豆豆单</a></li>\
+                                <li><a href="http://so.tudou.com/isearch/{word}">土豆视频</a></li>\
+                                <li><a href="http://so.youku.com/v?keyword={word}">优酷视频</a></li>\
+                                <li><a href="http://so.youku.com/search_playlist/q_{word}">优酷列表</a></li>\
+                                <li><a href="http://www.douban.com/group/search?q={word}">豆瓣小组</a></li>\
+                                <li><a href="http://www.google.com.hk/search?&q=ed2k+{word}">ED2K</a></li>\
+                                <li><a href="http://so.youku.com/v?keyword={word}">优酷视频</a></li>\
+                            </ul>');
+        },
+
+        show : function() {
+            var that = this;
+            clearTimeout(this.ID);
+            this.ID = setTimeout(function() {
+                that.list.fadeIn();
+                that.isShown = true;
+            }, 100);
+        },
+
+        hide : function() {
+            if(!this.isShown) return;
+            var that = this;
+            clearTimeout(this.ID);
+            this.ID = setTimeout(function() {
+                that.list.fadeOut();
+                that.isShown = false;
+            }, 100);
+        }
+
+    });
+
+    /**
+     * 猴子底部工具栏——提供回到顶部、回到底部等功能
      * updateTime : 2012-3-13
      */
     MonkeyModule('MonkeyBottomToolbar', {
@@ -2420,7 +2530,7 @@ typeof Updater != 'undefined' && new Updater({
                }',
 
         html : '<div id="MonkeyBottomToolbar">\
-                    <span class="Monkey-Button">广播</span>\
+                    <!--span class="Monkey-Button">广播</span-->\
                     <span class="Monkey-Button" monkey-action="GoDown" title="回到底部">向下</span>\
                     <span class="Monkey-Button" monkey-action="GoUp" title="回到顶部">向上</span>\
                 </div>',
@@ -2469,14 +2579,15 @@ typeof Updater != 'undefined' && new Updater({
 
             $(document.head).on('DOMNodeInserted', function(e) {
                 if(fm_js.test(e.target.src)) {                  //判断js是否加载
-                    eval('setTimeout(function() {\
-                        unsafeWindow._extStatusHandler = unsafeWindow.extStatusHandler;\
-                        log(unsafeWindow.extStatusHandler);\
-                        unsafeWindow.extStatusHandler = function(o) {\
-                            unsafeWindow._extStatusHandler(o);\
-                            that.fm(o);\
-                        }\
-                    }, 1000)')
+                    setTimeout(function() {
+                        eval('\
+                            unsafeWindow._extStatusHandler = unsafeWindow.extStatusHandler;\
+                            log(unsafeWindow.extStatusHandler);\
+                            unsafeWindow.extStatusHandler = function(o) {\
+                                unsafeWindow._extStatusHandler(o);\
+                                that.fm(o);\
+                        }')
+                    }, 1000)
                 }
             })
         },
