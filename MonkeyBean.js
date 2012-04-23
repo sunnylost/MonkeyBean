2dce31922470ba8b938dec33aaa688c22ae1711e
sunnylost <sunnylost@gmail.com>
2012-04-24 03:05:48 +0800

    版本号更新到1.0 +导航栏增加"未读豆邮"数目 +增加猴子爱问

--- MonkeyBean.js
+++ MonkeyBean.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           MonkeyBean
 // @namespace      sunnylost
-// @version        0.7
+// @version        1.0
 // @include        http://*.douban.*/*
 // @include        http://douban.fm/*
 // @require http://userscript-autoupdate-helper.googlecode.com/svn/trunk/autoupdatehelper.js
@@ -13,7 +13,7 @@
 typeof Updater != 'undefined' && new Updater({
     name: "MonkeyBean",
     id: "124760",
-    version:"0.7"
+    version:"1.0"
 }).check();
 
 /**
@@ -163,7 +163,7 @@ typeof Updater != 'undefined' && new Updater({
         path : location.hostname + location.pathname,
 
         //开启debug模式
-        debugMode : true,
+        debugMode : false,
 
         log : function(msg) {
             MonkeyBean.debugMode && typeof console !== 'undefined' && console.log(msg);
@@ -324,9 +324,6 @@ typeof Updater != 'undefined' && new Updater({
             }
         }
        
-
-        console.log('页面类型====' + type + '  翻页类型====' + turnType);
-        console.log('Path====' + path);
         return {
             'type' : type,
             'turnType' : turnType
@@ -1009,10 +1006,12 @@ typeof Updater != 'undefined' && new Updater({
         }
     });
 
-
+    
+    var unreadDouMail = $('.top-nav-info em').html();  //未读豆邮
+    unreadDouMail = unreadDouMail == null ? '' : unreadDouMail;
     /**
      * 猴子导航栏——用于显示顶部导航栏的二级菜单
-     * updateTime : 2012-3-10
+     * updateTime : 2012-4-24
      */
     MonkeyModule('MonkeyNavigator', {
         css : '.Monkey-Nav-top {\
@@ -1028,6 +1027,9 @@ typeof Updater != 'undefined' && new Updater({
             .Monkey-Nav-top a:hover {\
                 color : #566D5E;\
                 background-color : #fff;\
+            }\
+            .Monkey-Nav-top em {\
+                color: #1398B0;\
             }\
             .Monkey-Nav-bd {\
                 position : fixed;\
@@ -1173,10 +1175,10 @@ typeof Updater != 'undefined' && new Updater({
                        <div class="Monkey-Nav Monkey-Setting">\
                             <ul>\
                                 <li>\
-                                    <a href="http://www.douban.com/accounts/" target="_blank">我的帐号</a>\
+                                    <a href="http://www.douban.com/accounts/" target="_blank">我的帐号<em>' + unreadDouMail + '</em></a>\
                                     <ul>\
-                                        <li><a href="http://www.douban.com/doumail/">豆邮</a></li>\
-                                        <li><a href="javascript:void(0);" monkey-action="MonkeyConfig.config" title="MonkeyBean插件设置">MonkeyBean</a></li>\
+                                        <li><a href="http://www.douban.com/doumail/">豆邮<em>' + unreadDouMail + '</em></a></li>\
+                                        <!--li><a href="javascript:void(0);" monkey-action="MonkeyConfig.config" title="MonkeyBean插件设置">MonkeyBean</a></li-->\
                                         <li><a href="http://www.douban.com/accounts/logout?ck=' + MonkeyBean.getCk() + '">退出</a></li>\
                                     </ul>\
                                 </li>\
@@ -1353,6 +1355,7 @@ typeof Updater != 'undefined' && new Updater({
 
         render : function(type) {
             GM_addStyle(this.css.replace('{left}', this.navWidth()));
+            console.log($('.top-nav-info em').html());
             this.el.replaceWith(this.html);
             $('[name=Monkey-Nav-' + type + ']').addClass('on');
             $('.nav-srh').hide();//隐藏原始的搜索栏
@@ -1693,12 +1696,6 @@ typeof Updater != 'undefined' && new Updater({
                             <dl>\
                                 <dt>Q:为什么会写这个脚本？</dt>\
                                 <dd>A:不知道……</dd>\
-                                <dt>Q:为什么会写这个脚本？</dt>\
-                                <dd>A:不知道……</dd>\
-                                <dt>Q:为什么会写这个脚本？</dt>\
-                                <dd>A:不知道……</dd>\
-                                <dt>Q:我想提建议！加功能！</dt>\
-                                <dd>A:不知道……</dd>\
                             </dl>\
                         </div>\
                     </div>\
@@ -1824,6 +1821,7 @@ typeof Updater != 'undefined' && new Updater({
                 </div>',
 
         fit : function() {
+            return false;  //暂时屏蔽翻页功能
             this.paginator = $('div.paginator');
             this.current = this.paginator.find('.thispage'); 
             this.next = this.current.next(); 
@@ -2441,6 +2439,24 @@ typeof Updater != 'undefined' && new Updater({
 
         load : function() {
             var that = this;
+            this.keyword = $('span[property=v:itemreviewed]').html();
+            this.template = '<ul id="MonkeySearchList">\
+                                <li><a href="http://www.google.com.hk/search?&q={word}">Google</a></li>\
+                                <li><a href="http://www.baidu.com/s?ie=utf-8&wd={word}">百度一下</a></li>\
+                                <li><a href="http://www.verycd.com/search/folders/{word}">VeryCD</a></li>\
+                                <li><a href="http://zh.wikipedia.org/w/index.php?search={word}">维基百科</a></li>\
+                                <li><a href="http://books.google.com/books?q={word}">谷歌书籍</a></li>\
+                                <li><a href="http://shooter.cn/search/{word}">射手字幕</a></li>\
+                                <li><a href="http://wenku.baidu.com/search?word={word}">百度文库</a></li>\
+                                <li><a href="http://so.tudou.com/psearch/{word}">土豆豆单</a></li>\
+                                <li><a href="http://www.douban.com/group/search?q={word}">百度文库</a></li>\
+                                <li><a href="http://www.douban.com/group/search?q={word}">土豆豆单</a></li>\
+                                <li><a href="http://so.tudou.com/isearch/{word}">土豆视频</a></li>\
+                                <li><a href="http://so.youku.com/v?keyword={word}">优酷视频</a></li>\
+                                <li><a href="http://so.youku.com/search_playlist/q_{word}">优酷列表</a></li>\
+                                <li><a href="http://www.google.com.hk/search?&q=ed2k+{word}">ED2K</a></li>\
+                                <li><a href="http://so.youku.com/v?keyword={word}">优酷视频</a></li>\
+                            </ul>';
             this.render();
             $('#MonkeySearchBtn').hover(function() {
                 clearTimeout(that.ID);
@@ -2466,25 +2482,7 @@ typeof Updater != 'undefined' && new Updater({
             this.el = $(this.html);
             $('.aside').prepend(this.el);
             //var list = MonkeyBean.get('MonkeySearchList');
-            this.el.append('<ul id="MonkeySearchList">\
-                                <li><a href="http://www.google.com.hk/search?&q={word}">Google</a></li>\
-                                <li><a href="http://www.baidu.com/s?ie=utf-8&wd={word}">百度一下</a></li>\
-                                <li><a href="http://www.verycd.com/search/folders/{word}">VeryCD</a></li>\
-                                <li><a href="http://zh.wikipedia.org/w/index.php?search={word}">维基百科</a></li>\
-                                <li><a href="http://books.google.com/books?q={word}">谷歌书籍</a></li>\
-                                <li><a href="http://shooter.cn/search/{word}">射手字幕</a></li>\
-                                <li><a href="http://ishare.iask.sina.com.cn/search.php?key={gb:word}">新浪爱问</a></li>\
-                                <li><a href="http://wenku.baidu.com/search?word={gb:word}">百度文库</a></li>\
-                                <li><a href="http://so.tudou.com/psearch/{word}">土豆豆单</a></li>\
-                                <li><a href="http://www.douban.com/group/search?q={word}">百度文库</a></li>\
-                                <li><a href="http://www.douban.com/group/search?q={word}">土豆豆单</a></li>\
-                                <li><a href="http://so.tudou.com/isearch/{word}">土豆视频</a></li>\
-                                <li><a href="http://so.youku.com/v?keyword={word}">优酷视频</a></li>\
-                                <li><a href="http://so.youku.com/search_playlist/q_{word}">优酷列表</a></li>\
-                                <li><a href="http://www.douban.com/group/search?q={word}">豆瓣小组</a></li>\
-                                <li><a href="http://www.google.com.hk/search?&q=ed2k+{word}">ED2K</a></li>\
-                                <li><a href="http://so.youku.com/v?keyword={word}">优酷视频</a></li>\
-                            </ul>');
+            this.el.append(this.template.replace(/\{word\}/g, this.keyword));
         },
 
         show : function() {
@@ -2506,6 +2504,137 @@ typeof Updater != 'undefined' && new Updater({
             }, 100);
         }
 
+    });
+
+    //猴子爱问——代码来自于另外一个脚本：爱问共享资料豆瓣插件+，代码风格不统一，暂时不改了
+    MonkeyModule('MonkeyIAsk', {
+        'fit' : function() {
+            var type = MonkeyBean.page.type;
+            return (type == 'book' || type == 'movie' || type == 'music') && MonkeyBean.path.indexOf('subject') != -1;
+        },
+
+        load : function() {
+            this.isakFunction();
+        },
+
+        isakFunction : function() {
+
+            var title = $('html head title').text(),
+                styleContent = '.douban_iask_format{overflow:hidden; font-size:12px; margin:-10px 0 2px;} ' +
+                               '.douban_iask_format li{ float:left; margin:0 2px 0; padding:0 3px 0; border-radius:4px; border:1px solid #ccc; cursor:pointer;}' +
+                               '.douban_iask_format li.check {background-color:#a8c598;}',
+                keyword1 = title.replace( '(豆瓣)', '' ).trim(),
+                keyword2 = encodeURIComponent( keyword1 ),
+                formatLabel = '<ul class="douban_iask_format"><li class="check">全部</li><li>TXT</li><li>DOC</li><li>PDF</li><li>PPT</li><li>CHM</li><li>RAR</li><li>EXE</li></ul>',
+                format = '',
+                html_title = '<div class="da3" style="margin-bottom:0;padding-bottom:10px;overflow: hidden;"><dl><dt style="display:inline;"><img src="http://iask.com/favicon.ico" width="15px;" height="15px;" style="margin-bottom:-2px;" /> <b><a href="http://ishare.iask.sina.com.cn/?from=douban" target="_blank">爱问共享资料</a>推荐：</b></dt><a href="http://ishare.iask.sina.com.cn/upload/?from=douban" target="_blank">我要分享资料</a><dd>最大的中文在线资料分享站，数百万资料免费下载</dd></dl>' + formatLabel + '</div>',
+                html_body_start = '<div class="indent" id="db-doulist-section" style="padding-left:5px;border:1px #F4F4EC solid;"><ul class="bs" style="clear:left;">',
+                html_body_no = '<li>没有找到相关资料，<a href="http://ishare.iask.sina.com.cn/upload/?from=douban" title="资料上传" target="_blank"><b>立即上传</b></a>即可与豆友们分享！</li>',
+                html_body_end = '</ul>',
+                html_body_endmore = '<div style="text-align:right; padding:5px 10px 5px 0px;"><a href="http://api.iask.sina.com.cn/api/search2.php?key=' + keyword2 + '&from=douban&format=" target="_blank">更多&hellip;</a></div>',
+                html_body_endend = '</div>',
+                length = 30,
+                unitname = new Array('Y','Z','E','P','T','G','M','K'),
+                unitsize = new Array(1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024,1024 * 1024,1024);
+            var iask = {
+                format : '',
+                url : 'http://api.iask.sina.com.cn/api/isharesearch.php?key=' + keyword2 + '&datatype=json&start=0&num=5&keycharset=utf8&format=',
+
+                init : function() {
+                    var that = this;
+                    that.addStyle(styleContent);
+                    $.ajax({
+                        type : 'GET',
+                        dataType : 'script',
+                        url : that.url,
+                        success : function() {
+                            var result = unsafeWindow.iaskSearchResult;
+                            if( result.sp.m > 0 ) {
+                                var result = that.generate(result);
+                                $(html_title + html_body_start + result + html_body_end + html_body_endmore + html_body_endend ).insertAfter($('#MonkeySearch'));
+                                //$( '.aside' ).prepend( html_title + html_body_start + result + html_body_end + html_body_endmore + html_body_endend );
+                                that.bindListener();
+                                that.content = $('#db-doulist-section ul')[0];
+                                that.contentP = that.content.parentNode;
+                                that.checked = $('.douban_iask_format .check')[0];
+                            } else {
+                                $(html_title + html_body_start + html_body_no + html_body_end + html_body_endend).insertAfter($('#MonkeySearch'));
+                                //$( '.aside' ).prepend( html_title + html_body_start + html_body_no + html_body_end + html_body_endend );
+                            }
+                        }
+                    });
+                },
+
+                addStyle : function(style) {
+                    var s = document.createElement('style');
+                    s.type = 'text/css';
+                    s.innerHTML = style;
+                    document.getElementsByTagName('head')[0].appendChild(s);
+                },
+
+                bindListener : function() {
+                    var that = this;
+                    //绑定事件
+                    $('.douban_iask_format').delegate('li', 'click', function(e) {
+                        var tar = e.target;
+                        that.format = tar.innerHTML == '全部' ? '' : tar.innerHTML;
+                        if(that.checked != tar) {
+                            that.checked.className = '';
+                            that.checked = tar;
+                            tar.className = 'check';
+                        }
+                        that.getContent(that.url + that.format);
+                    });
+                },
+
+                generate : function(iaskSearchResult) {
+                    var title,title2, image, filesize, url, unit, html_body_yes='';
+                    var regex = /([A-Z\u0391-\uffe5])/g,
+                        searchResult = iaskSearchResult.sp.result;
+
+                    for( var resultIndex = 0,resultTotal = searchResult.length; resultIndex < resultTotal; resultIndex++) {
+                         var result = searchResult[resultIndex];
+                            title = result.title;
+                            title2 = title.replace( regex, "$1*" );
+                            ellipsis = title2.length > length ? '..' : '' ;
+                            title2 = title2.substr( 0, length ).replace( /\*/g, '' ) + ellipsis;
+                            image = result.format;
+                            filesize = result.filesize;
+                            if( filesize < 1024 ) filesize = filesize+'B';
+                            for( var i=0; i<unitname.length; i++ ){
+                                if( filesize > unitsize[i] || filesize==unitsize[i] ){
+                                    filesize = Math.round( filesize / unitsize[i] * 10 ) / 10 + unitname[i];
+                                }
+                            }
+                            url = result.url;
+                            html_body_yes += '<li><img src="http://www.sinaimg.cn/pfp/ask/images/'
+                                             + image + '.gif" style="margin-bottom:-2px;" /> <a href="' + url + '?from=douban" title="' + title + '" target="_blank">'
+                                             + title2 + '</a><span class="pl">(大小:' + filesize + ' 积分:' + result.price + ')</span></li>';
+                    }
+                    return html_body_yes;
+                },
+
+                getContent : function(url) {
+                    var that = this;
+                    $.ajax({
+                        type : 'GET',
+                        dataType : 'script',
+                        url : url,
+                        success : function() {
+                            if( iaskSearchResult.sp.m > 0 ) {
+                                var result = that.generate(iaskSearchResult);
+                                that.contentP.style.display = 'block';
+                                that.content.innerHTML = '';
+                                that.content.innerHTML = result;
+                            } else {
+                                that.content.innerHTML = html_body_no;
+                            }
+                        }
+                    });
+                }
+            };
+            iask.init();
+        }
     });
 
     /**
